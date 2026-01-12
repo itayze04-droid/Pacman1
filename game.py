a6ae5ecf128b38420d6e5a914503ee1a86292652
yarin42 <yarinyarin4321@gmail.com>
2026-01-12 18:46:43 +0200

    added PacmanGame

--- game.py
+++ game.py
@@ -1,6 +1,17 @@
+import arcade
 """
 מודול הלוגיקה הראשית של משחק הפקמן.
 
 מכיל את המחלקה:
 - PacmanGame: ניהול מצב המשחק, ציור, עדכון ותשובת מקלדת.
 """
+class PacmanGame(arcade.View):
+    def __init__(self, object_list=None, player=None, game_over=False):
+        super().__init__()
+        self.list_wall = []
+        self.list_coin = []
+        self.list_ghost = []
+        self.list_player = []
+        self.object_list = object_list
+        self.player = player
+        self.game_over = game_over
