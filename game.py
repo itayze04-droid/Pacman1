347f0b0c46545d1d1976240a8b404a2758fb24f3
itayze04-droid <itayze04@gmail.com>
2026-01-19 18:56:26 +0200

    added on_updatde

--- game.py
+++ game.py
@@ -1,4 +1,7 @@
 import arcade
+from pymunk.examples.spiderweb import on_key_press
+
+import characters
 from constants import LEVEL_MAP
 from constants import TILE_SIZE
 from constants import rows
@@ -56,4 +59,55 @@ class PacmanGame(arcade.View):
         arcade.draw_text(f"Score: {self.player.score}",10,self.window.height - 30,arcade.color.WHITE,16)
         arcade.draw_text(f"Lives: {self.player.lives}",10,self.window.height - 55,arcade.color.WHITE,16)
         if self.game_over:
-            arcade.draw_text("GAME OVER",self.window.width / 2,self.window.height / 2,arcade.color.RED,40,)
+            arcade.draw_text("GAME OVER",self.window.width / 2,self.window.height / 2,arcade.color.RED,40)
+
+
+   def on_update(self,delta_time):
+       if self.game_over:
+            return
+
+       pre_x=characters.Player.center_x
+       pre_y=characters.Player.center_y
+       self.Player.update()
+
+
+       if arcade.check_for_collision_with_list(self.Player,self.list_wall):
+           self.Player.center_x = pre_x
+           self.Player.center_y = pre_y
+
+       hit_coins= arcade.check_for_collision_with_list(self.Player,self.list_coin)
+       for coin in hit_coins:
+           self.Player.score+=1
+           coin.remove_from_sprite_lists()
+
+       for ghost in self.list_ghost:
+           ghost_old_x=ghost.center_x
+           ghost_old_y=ghost.center_y
+           ghost.update(delta_time)
+
+           if arcade.check_for_collision_with_list(ghost,self.list_wall):
+               ghost.center_x=ghost_old_x
+               ghost.center_y=ghost_old_y
+               ghost.pick_new_direction()
+
+
+
+       if arcade.check_for_collision_with_list(self.Player,self.list_ghost):
+           self.Player.lives-=1
+           self.Player.center_x=self.start_x
+           self.Player.center_y = self.start_y
+           self.Player.change_x=0
+           self.Player.change_y=0
+
+           if self.Player.lives<=0:
+               self.game_over=True
+
+
+
+
+
+
+
+
+
+
