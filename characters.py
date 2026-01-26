fdfc1bccc64565d02c00e36fbc01caed41f0d119
yarin42 <yarinyarin4321@gmail.com>
2026-01-26 19:16:29 +0200

    added better map, apple , and teleportation

--- characters.py
+++ characters.py
@@ -1,17 +1,6 @@
-"""
-מודול הדמויות (Sprites) במשחק פקמן.
-
-מכיל את המחלקות:
-- Character: מחלקת בסיס לדמויות (שיתוף שדה speed וכו')
-- Pacman: השחקן הראשי
-- Ghost: רוחות שנעות בצורה רנדומלית
-- Coin: מטבעות לאיסוף
-- Wall: קירות שחוסמים תנועה
-"""
 import random
 import arcade
 from constants import TILE_SIZE
-
 
 class Coin(arcade.Sprite):
     def __init__(self, x, y):
@@ -24,6 +13,13 @@ class Wall(arcade.Sprite):
     def __init__(self, x, y):
         super().__init__()
         self.texture = arcade.make_soft_square_texture(TILE_SIZE, arcade.color.BLUE, 255)
+        self.center_x = x
+        self.center_y = y
+
+class Apple(arcade.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.texture = arcade.make_circle_texture(TILE_SIZE//2 - 4, arcade.color.RED)
         self.center_x = x
         self.center_y = y
 
@@ -43,25 +39,36 @@ class Player(Character):
         super().__init__(x, y, player_texture)
         self.score = 0
         self.lives = 3
+        self.power_mode = False
+        self.power_time = 0.0
 
     def update(self):
         self.center_x += self.change_x * self.speed
         self.center_y += self.change_y * self.speed
 
 enemy_texture = arcade.make_soft_square_texture(TILE_SIZE-6, arcade.color.RED, 255)
+ghost_power_texture = arcade.make_soft_square_texture(TILE_SIZE-6, arcade.color.LIGHT_BLUE, 255)
 class Enemy(Character):
     def __init__(self, x, y):
         super().__init__(x, y, enemy_texture)
         self.direction_change_time = 0
 
     def pick_new_direction(self):
-        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        directions = [(0,1),(0,-1),(1,0),(-1,0)]
         self.change_x, self.change_y = random.choice(directions)
         self.direction_change_time = random.uniform(0.3, 1.0)
 
-    def update(self, delta_time=1/60):
+    def update(self, delta_time=1/60, power_mode=False):
         self.direction_change_time -= delta_time
         if self.direction_change_time <= 0:
             self.pick_new_direction()
-        self.center_x += self.change_x * self.speed
-        self.center_y += self.change_y * self.speed
+
+        if power_mode:
+            self.texture = ghost_power_texture
+            speed_multiplier = 0.5
+        else:
+            self.texture = enemy_texture
+            speed_multiplier = 1.0
+
+        self.center_x += self.change_x * self.speed * speed_multiplier
+        self.center_y += self.change_y * self.speed * speed_multiplier
